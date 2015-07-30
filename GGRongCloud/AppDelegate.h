439092d36355e7a6f7f4290b091d1a8c3a02d2e2
zero <osmk@qq.com>
2015-07-30 11:01:11 +0800

    Initial Commit

---
+++ GGRongCloud/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GGRongCloud
+//
+//  Created by __无邪_ on 15/7/30.
+//  Copyright © 2015年 __无邪_. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
